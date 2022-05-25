3aed55ced7ca722d5c7373d7ba0d6c4ff30eb964
vijaykumar <vijay1111mane@gmail.com>
2022-05-25 10:22:19 +0530

    commite;

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Hooks />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Hooks from './components/Hooks'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    Hooks
   }
 }
 </script>
---
+++ src/components/Hooks.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <h1>Life Cycle hooks of vue js</h1>
+</div>
+</template>
+
+<script>
+
+export default {
+   name: "Life_cycle-Hooks",
+}
+</script>
